d286acf7d3bad9ccaee9671e40247bd059e82de9
Fırat Uçar <firatucar94@gmail.com>
2021-01-12 17:50:28 +0300

    MVCKategoriyeGoreKitap Added.

---
+++ MVC_Kitaplık/MVC_Kitaplık/Controllers/KitapController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Kitaplık.Models;
+
+namespace MVC_Kitaplık.Controllers
+{
+    public class KitapController : Controller
+    {
+        // GET: Kitap
+        Db_KitapEntities db = new Db_KitapEntities();
+        public ActionResult Index()
+        {
+            var kitaplar = db.Tbl_Kitap.ToList();
+            return View(kitaplar);
+        }
+
+        [HttpGet]
+        public ActionResult KitapEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KitapEkle(Tbl_Kitap tbl_Kitap)
+        {
+            
+            db.Tbl_Kitap.Add(tbl_Kitap);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KitapSil(int id)
+        {
+            var kitap = db.Tbl_Kitap.Find(id);
+            db.Tbl_Kitap.Remove(kitap);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KitapGetir(int id)
+        {
+            var kitap = db.Tbl_Kitap.Find(id);
+            return View("KitapGetir", kitap);
+
+        }
+
+        public ActionResult KitapGuncelle(Tbl_Kitap t)
+        {
+            var kitap = db.Tbl_Kitap.Find(t.KitapId);
+            kitap.Ad = t.Ad;
+            kitap.Yazar = t.Yazar;
+            kitap.Sayfa = t.Sayfa;
+            kitap.Kategori = t.Kategori;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
